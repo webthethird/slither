71da4a9c12ff00a9f19e100b9f371a080f6b7146
webthethird <ratzbodell@gmail.com>
2022-12-16 11:58:05 -0600

    Check for new functions in V2

--- slither/tools/upgradeability/checks/diff_with_v2.py
+++ slither/tools/upgradeability/checks/diff_with_v2.py
@@ -45,12 +45,61 @@ class DiffContractV1ContractV2(AbstractCheck):
         contract2 = self._contract2()
         order_vars1 = [variable for variable in contract1.state_variables if not variable.is_constant]
         order_vars2 = [variable for variable in contract2.state_variables if not variable.is_constant]
+        func_sigs1 = [function.solidity_signature for function in contract1.functions]
+        func_sigs2 = [function.solidity_signature for function in contract2.functions]
 
         results = []
 
         if len(order_vars2) <= len(order_vars1):
             # Handle by MissingVariable
             return results
+
+        new_modified_functions = []
+        for sig in func_sigs2:
+            if sig not in func_sigs1:
+                function = contract2.get_function_from_signature(sig)
+                new_modified_functions.append(function)
+                info = [
+                    "New function in ",
+                    contract2,
+                    "\n",
+                ]
+            # elif ...:
+            #     TODO: Determine if a function has been modified
+            else:
+                continue
+            info += ["\t ", function, "\n"]
+            state_variables_read = function.state_variables_read
+            state_variables_written = function.state_variables_written
+            additional_fields = {
+                "signature": sig,
+                "visibility": function.visibility,
+                "state_variables_read": [var.canonical_name for var in state_variables_read],
+                "state_variables_written": [var.canonical_name for var in state_variables_written]
+            }
+            json = self.generate_result(info, additional_fields)
+            results.append(json)
+
+        # List unmodified functions that call new/modified functions
+        for function in contract2.functions:
+            if function in new_modified_functions:
+                continue
+            modified_calls = [func for func in new_modified_functions if func in function.internal_calls]
+            if len(modified_calls) > 0:
+                info = [
+                    "Call to new/modified function(s) by unmodified function ",
+                    function,
+                    "\n",
+                ]
+                for func in modified_calls:
+                    info += ["\t ", func, "\n"]
+                additional_fields = {
+                    "signature": function.solidity_signature,
+                    "visibility": function.visibility,
+                    "new/modified calls": [func.solidity_signature for func in modified_calls]
+                }
+                json = self.generate_result(info, additional_fields)
+                results.append(json)
 
         for idx, var in enumerate(order_vars2):
             # Handle incorrect variable order in DifferentVariableContractNewContract
